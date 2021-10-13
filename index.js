c12d434ecc3e4815733af885d9aec7ee894fae32
+ <brandi.ude@gmail.com>
2021-10-12 23:19:15 -0400

    commit tree 59b71b10495ea497707d225b7c368c23aaacfc9a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+let num3 = 16
+let num4 = 4
+let mod = num3 / num4;
+
+let max = Math.max(2, 4, 6, 8, 20);
